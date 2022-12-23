3f8889690a786d6836653aa6615691320990f77e
cfyasim <asim.khan@contify.com>
2022-12-23 11:10:14 +0530

    added summarization api

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from logging import config as logger_config
 from pathlib import Path
 import config
-from serializers import BertText
+from serializers import BertText, SummaryText
 import numpy as np
 import torch
 from functools import lru_cache
-from transformers import AutoModelForSequenceClassification, Trainer, AutoTokenizer
-from constants import MODEL_FILE_NAME
+from transformers import (AutoModelForSequenceClassification,
+                          Trainer,
+                          AutoTokenizer,
+                          AutoModelForSeq2SeqLM)
+from constants import MODEL_FILE_NAME, SUMMARY_MODEL_FILE_NAME
 
 
 @lru_cache()
@@ -26,6 +29,18 @@ def get_bert_classifier():
     return topic_model, topic_trainer, topic_tokenizer
 
 
+@lru_cache()
+def get_bert_summarizer():
+    summary_tokenizer = AutoTokenizer.from_pretrained(
+        f"./{SUMMARY_MODEL_FILE_NAME}"
+    )
+    summary_model = AutoModelForSeq2SeqLM.from_pretrained(
+        f"./{SUMMARY_MODEL_FILE_NAME}"
+        f"")
+    return summary_tokenizer, summary_model
+
+
+summarization_tokenizer, summarization_model = get_bert_summarizer()
 model, trainer, tokenizer = get_bert_classifier()
 app = FastAPI(docs_url=None, redoc_url=None)
 security = HTTPBasic()
@@ -64,7 +79,8 @@ def is_authenticated_user(
 
 
 @app.post('/predict/topic/')
-async def predict_topic(story: BertText, auth_status: int = Depends(is_authenticated_user)):
+async def predict_topic(story: BertText,
+                        auth_status: int = Depends(is_authenticated_user)):
     """This api is used to tag Topic from Text.
 
     params: story: BertText
@@ -96,5 +112,43 @@ async def predict_topic(story: BertText, auth_status: int = Depends(is_authentic
             f" Error: {err} , Traceback: {traceback.format_exc()}")
 
 
+@app.post('/predict/summarize_text/')
+async def summarize_text(story: SummaryText,
+                         auth_status: int = Depends(is_authenticated_user)):
+    """This tes-api is used to tag ner from Text.
+    params: story: SummaryText
+    Return: Summarized text
+    """
+    summary_text = ""
+    try:
+        data = story.dict()
+        data_dict = data['data']
+        summary_text = data_dict["text"]
+        max_len = data_dict["max_len"]
+        no_of_beams = data_dict["no_of_beams"]
+        inputs = summarization_tokenizer(
+            [summary_text],
+            max_length=1024,
+            return_tensors="pt",
+            truncation=True
+        )
+        # Generate Summary
+        summary_ids = summarization_model.generate(inputs["input_ids"],
+                                                   num_beams=no_of_beams,
+                                                   min_length=0,
+                                                   max_length=max_len)
+        bert_output = summarization_tokenizer.batch_decode(
+            summary_ids,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False
+            )
+        return bert_output[0]
+
+    except Exception as err:
+        logger.info(f"Ner Bert: Error occurred for story {summary_text} "
+                    f" Error: {err} , Traceback: {traceback.format_exc()}")
+
 if __name__ == '__main__':
-    uvicorn.run(f"{Path(__file__).stem}:app", port=8082, host='localhost', reload=True)
+    uvicorn.run(
+        f"{Path(__file__).stem}:app", port=8082, host='localhost', reload=True
+    )
--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
 MODEL_FILE_NAME = "bert-fine-tuned-sem_eval-english"
+SUMMARY_MODEL_FILE_NAME = "distilbart-cnn-12-6"
